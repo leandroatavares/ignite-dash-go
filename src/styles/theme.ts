42a8658376a5e717e336ff9e07a993f96648a220
leandroatavares <leandroanjostavares@gmail.com>
2022-03-22 20:59:53 -0300

    configura thema

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,17 +3,21 @@ import { extendTheme } from '@chakra-ui/react';
 export const theme = extendTheme({
   colors: {
     gray: {
-      "900": "#181823",
-      "800": "#1f2029",
-      "700": "#353646",
-      "600": "#4b4d63",
-      "500": "#616488",
-      "400": "#797d9a",
-      "300": "#9699b0",
-      "200": "#b3b5c6",
-      "100": "#d1d2dc",
-      "50": "#eeeef2"
-    }
+      50: '#EEEEF2',
+      100: '#D1D2DC',
+      200: '#B3B5C6',
+      400: '#797D9A',
+      300: '#9699B0',
+      500: '#616480',
+      600: '#4B4D63',
+      700: '#353646',
+      800: '#1F2029',
+      900: '#181B23'
+    },
+  },
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto'
   },
   styles: {
     global: {
