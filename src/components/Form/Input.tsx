98f793ef7a8d257b0e13a17a75a7c21c52e3492c
leandroatavares <leandroanjostavares@gmail.com>
2022-04-01 19:51:29 -0300

    separa componente Input da page SignIn

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,31 @@
+import {
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChacraInputProps
+} from '@chakra-ui/react';
+
+interface InputProps extends ChacraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        name={name}
+        id={name}
+        focusBorderColor='pink.500'
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: "gray.900"
+        }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { Button, Flex, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react'
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex
       w="100vw"
@@ -17,37 +18,8 @@ export default function Home() {
         flexDir="column"
       >
         <Stack spacing="4">
-          <FormControl>
-            <FormLabel htmlFor='email'>E-mail</FormLabel>
-            <Input
-              name='email'
-              type="text"
-              placeholder='email'
-              focusBorderColor='pink.500'
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: "gray.900"
-              }}
-              size="lg"
-            />
-          </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor='password'>Password</FormLabel>
-            <Input
-              name='password'
-              type="password"
-              placeholder='password'
-              focusBorderColor='pink.500'
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: "gray.900"
-              }}
-              size="lg"
-            />
-          </FormControl>
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
         </Stack>
         <Button type='submit' mt="6" colorScheme="pink">Entrar</Button>
       </Flex>
